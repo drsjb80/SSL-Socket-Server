67d7773d8ba8b20f00ca9bf5dc9c39e4fd092bd6
drsjb80 <drsjb80@gmail.com>
2019-06-29 19:24:32 -0600

    Lot of simplification.

--- src/edu/msudenver/SSLSocketServer.java
+++ src/edu/msudenver/SSLSocketServer.java
@@ -11,57 +11,27 @@ import java.io.*;
 
 
 public class SSLSocketServer {
-    public static void main(String[] args) {
-        int intSSLport = 4443;
+    public static void main(String[] args) throws IOException {
+        int intSSLport = 443;
 
         System.setProperty("javax.net.ssl.keyStore","testkeystore.ks");
         System.setProperty("javax.net.ssl.keyStorePassword","testpwd");
-
         // System.setProperty("javax.net.debug","all");
 
         SSLServerSocketFactory sslServerSocketfactory = (SSLServerSocketFactory)SSLServerSocketFactory.getDefault();
-        SSLServerSocket sslServerSocket;
-        SSLSocket sslSocket;
+        SSLServerSocket sslServerSocket = (SSLServerSocket) sslServerSocketfactory.createServerSocket(intSSLport);
+        SSLSocket sslSocket = (SSLSocket) sslServerSocket.accept();
 
-        try {
-            sslServerSocket = (SSLServerSocket) sslServerSocketfactory.createServerSocket(intSSLport);
-            sslSocket = (SSLSocket) sslServerSocket.accept();
-        } catch (IOException e) {
-            e.printStackTrace();
-            return;
-        }
+        BufferedReader in = new BufferedReader(new InputStreamReader(sslSocket.getInputStream()));
+        BufferedWriter out = new BufferedWriter(new OutputStreamWriter(sslSocket.getOutputStream()));
 
-        while (true) {
-            BufferedWriter out;
-            BufferedReader in;
-            try {
-                in = new BufferedReader(new InputStreamReader(sslSocket.getInputStream()));
-                out = new BufferedWriter(new OutputStreamWriter(sslSocket.getOutputStream()));
-            } catch (IOException e) {
-                e.printStackTrace();
-                break;
-            }
-            String inputLine;
+        String inputLine = in.readLine();
+        System.out.println("RECEIVED: " + inputLine);
+        if (inputLine != null) out.write(inputLine);
 
-            try {
-                while ((inputLine = in.readLine()) != null) {
-                    out.write(inputLine);
-                    System.out.println(inputLine);
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-                break;
-            }
-
-            try {
-                in.close();
-                out.close();
-                sslSocket.close();
-                sslServerSocket.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-                break;
-            }
-        }
+        in.close();
+        out.close();
+        sslSocket.close();
+        sslServerSocket.close();
     }
 }
